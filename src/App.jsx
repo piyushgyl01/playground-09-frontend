8f428e8f12974abf90051c50201b76679f228b73
piyush.gyl <piyush2022ug@gmail.com>
2025-02-05 16:05:14 +0530

    fix: removed teachr route

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ export default function App() {
     <>
       <main className="container text-center my-4">
         <h1 className="mt-5">Manage Students</h1>
-        <Link to={"/students"} className="btn btn-primary">View Students</Link>
-         <h1 className="mt-5">Manage Teachers</h1>
-        <Link to={"/teachers"} className="btn btn-primary">View Teachers</Link>
-         <h1 className="mt-5">Manage School</h1>
-        <Link to={"/school"} className="btn btn-primary">View School</Link>
+        <Link to={"/students"} className="btn btn-primary">
+          View Students
+        </Link>
+        <h1 className="mt-5">Manage School</h1>
+        <Link to={"/school"} className="btn btn-primary">
+          View School
+        </Link>
       </main>
     </>
   );
